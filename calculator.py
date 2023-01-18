e968b85fd264856171eccbb138cc2b19f2153cb7
bhupendra592 <bhupendra.jmd@gmail.com>
2023-01-18 15:48:59 +0530

    added Multiplication feature

--- calculator.py
+++ calculator.py
@@ -4,7 +4,27 @@ def add(num1,num2):
 def sub(num1,num2):
     return num1-num2
 
+def mul(num1,num2):
+    return num1*num2
+
+
 res_add = add(10,20)
 print(res_add)
 res_sub = sub(20,10)
 print(res_sub)
+res_mul = sub(20,10)
+print(res_mul)
+
+ feature-mul
+  master
+BHIoT$ ls
+calculator.py  LICENSE  README.md
+BHIoT$ git status
+On branch feature-mul
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	modified:   calculator.py
+
+no changes added to commit (use "git add" and/or "git commit -a")
+
