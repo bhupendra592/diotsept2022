831888e1c999ecd2cf8306a7dd41083243f00aaf
bhupendra592 <bhupendra.jmd@gmail.com>
2023-01-19 14:13:47 +0530

    added division feature

--- calculator.py
+++ calculator.py
@@ -7,9 +7,18 @@ def sub(num1,num2):
 def mul(num1,num2):
     return num1*num2
 
+def div(num1,num2):
+    try:
+        return num1/num2
+    except ZeroDivisionError:
+        print("Division by zero not allowed")
 res_add = add(10,20)
 print(res_add)
 res_sub = sub(20,10)
 print(res_sub)
-res_mul = sub(20,10)
+res_mul = mul(20,10)
 print(res_mul)
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+res_div = div(num1,num2)
+print(f"{num1} / {num2} = {res_div}")
